c8945c67a6e524bf555a63e8a52264d81832d290
Robertjo97 <robertjo97@gmail.com>
2022-11-18 13:04:51 -0800

    working on conversions

--- CSCI-113 Simulation Project/Source.cpp
+++ CSCI-113 Simulation Project/Source.cpp
@@ -6,22 +6,34 @@
 using namespace std;
 
 struct cache_block {
-	bool valid_bit;
+	bool valid_bit = false;
 	vector <int> tag;
 	vector <int> data;
 };
 
 int main() {
-	string instruction;
+	vector <cache_block> cache(16);
+	string line;
 	ifstream file;
+	vector<int> test;
 
 	file.open("02-Input-object-code");
-	while (!file.eof()) {
-		getline(file, instruction);
-		cout << instruction << endl << endl;
+	getline(file, line);
+	for (int i = 0; i < 32; i++) {
+		test.push_back(line[i] - '0');
 	}
-	file.close();
+	for (int i = 0; i < test.size(); i++) {
+		cout << test.at(i);
+	}
+	/*cout << line[0];
+	int n = line[0] - '0';
+	cout << n;
+	while (!file.eof()) {
+		
+		
+	}
+	file.close();*/
 
-	vector <cache_block> cache(16);
+	
 	return 0;
 }
