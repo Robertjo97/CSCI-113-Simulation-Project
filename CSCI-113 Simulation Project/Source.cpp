8b3213eeaea456a7cb422431bc9e19dcfb363f23
Robertjo97 <robertjo97@gmail.com>
2022-11-18 12:01:21 -0800

    file_I/O

--- CSCI-113 Simulation Project/Source.cpp
+++ CSCI-113 Simulation Project/Source.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+
+struct cache_block {
+	bool valid_bit;
+	vector <int> tag;
+	vector <int> data;
+};
 
 using namespace std;
 
 int main() {
+	string instruction;
+	ifstream file;
 
+	file.open("02-Input-object-code");
+	while (!file.eof()) {
+		getline(file, instruction);
+		cout << instruction << endl << endl;
+	}
+	file.close();
+
+	vector <cache_block> cache(16);
 	return 0;
 }
