458b1d08f4df4fee096d2bd506423b870a166d28
Robertjo97 <robertjo97@gmail.com>
2022-11-27 22:29:44 -0800

    All finished

--- CSCI-113 Simulation Project/Source.cpp
+++ CSCI-113 Simulation Project/Source.cpp
@@ -2,9 +2,16 @@
 #include <fstream>
 #include <string>
 
+/*
+Robert Orlando
+11/27/2022
+CSCI-113
+Memory Simulation Project
+*/
+
 using namespace std;
 
-struct cache_block {
+struct cache_block {				//Creates cache block structure
 	int set = 0;
 	int valid_bit = 0;
 	int history = 0;
@@ -12,24 +19,24 @@ struct cache_block {
 	int data = 0;
 };
 
-struct cache {
+struct cache {						//Creates 2 way set-associative cache
 	cache_block block_0[8];
 	cache_block block_1[8];
 };
 
-struct instruction {
+struct instruction {				//Holds instruction information
 	int op = 0;
 	int rs = 0;
 	int rt = 0;
 	int offset = 0;
 };
 
-int compute_mem_addr(instruction instr) {
+int compute_mem_addr(instruction instr) {			//Computes memory address from instruction information
 	int byte_addr = instr.rs + instr.offset;
 	int word_addr = byte_addr / 4;
 	return word_addr;
 }
-int conversion(int array[], int len) {
+int conversion(int array[], int len) {				//Converts binary arrays into decimal integers
 	int output = 0;
 	int power = 1;
 
@@ -42,64 +49,55 @@ int conversion(int array[], int len) {
 	return output;
 }
 
-void FourBitBin(int n)
+void FourBitBin(int n)								//Converts decimal integers into 4 bit binary numbers
 {
-	// array to store binary number
 	int binaryNum[32] = { };
 
-	// counter for binary array
 	int i = 0;
 	while (n > 0) {
 
-		// storing remainder in binary array
 		binaryNum[i] = n % 2;
 		n = n / 2;
 		i++;
 	}
-	// printing binary array in reverse order
+
 	for (int j = 3; j >= 0; j--)
 		cout << binaryNum[j];
 }
 
-void ThirtyTwoBitBin(int n)
+void ThirtyTwoBitBin(int n)							//Converts decimal integers into 32 bit binary numbers
 {
-	// array to store binary number
 	int binaryNum[32] = { };
 
-	// counter for binary array
 	int i = 0;
 	while (n > 0) {
 
-		// storing remainder in binary array
 		binaryNum[i] = n % 2;
 		n = n / 2;
 		i++;
 	}
-	// printing binary array in reverse order
+
 	for (int j = 31; j >= 0; j--)
 		cout << binaryNum[j];
 }
 
-void SevenBitBin(int n)
+void SevenBitBin(int n)								//Converts decimal integers into 7 bit binary numbers
 {
-	// array to store binary number
 	int binaryNum[32] = { };
 
-	// counter for binary array
 	int i = 0;
 	while (n > 0) {
 
-		// storing remainder in binary array
 		binaryNum[i] = n % 2;
 		n = n / 2;
 		i++;
 	}
-	// printing binary array in reverse order
+
 	for (int j = 6; j >= 0; j--)
 		cout << binaryNum[j];
 }
 
-void load_operation(int memory_address, int &rt, int (&regs)[8], cache& cache, int (&memory)[128]) {
+void load_operation(int memory_address, int &rt, int (&regs)[8], cache& cache, int (&memory)[128]) {	//Load operation function. Contains all logic for LW instruction.
 
 	int set = memory_address % 8;
 	int tag = memory_address / 8;
@@ -171,7 +169,7 @@ void load_operation(int memory_address, int &rt, int (&regs)[8], cache& cache, i
 
 }
 
-void store_operation(int memory_address, int& rt, int(&regs)[8], cache& cache, int(&memory)[128]) {
+void store_operation(int memory_address, int& rt, int(&regs)[8], cache& cache, int(&memory)[128]) {	//Store operation function. Contains all logic for SW instruction.
 	int set = memory_address % 8;
 	int tag = memory_address / 8;
 
@@ -190,14 +188,13 @@ void store_operation(int memory_address, int& rt, int(&regs)[8], cache& cache, i
 				cache.block_0[set].history = 0;
 			}
 			else if (cache.block_1[set].tag != tag) {			//block_0 [valid passed] [tag failed] block_1 [valid passed] [tag failed]
-				//LRU WB stuff
 				cout << "\thit";
 				if (cache.block_0[set].history == 0) {
 					memory[memory_address] = cache.block_0[set].data;
 					cache.block_0[set].data = regs[rt - 16];
 					cache.block_0[set].history = 1;
 					cache.block_1[set].history = 0;
-				}
+				}													//LRU WB
 				else if (cache.block_1[set].history == 0) {
 					memory[memory_address] = cache.block_1[set].data;
 					cache.block_1[set].data = regs[rt - 16];
@@ -224,8 +221,8 @@ void store_operation(int memory_address, int& rt, int(&regs)[8], cache& cache, i
 }
 
 
-instruction decode(string instr) {
-	instruction instruction_decoded;
+instruction decode(string instr) {				//Instruction decode function. 
+	instruction instruction_decoded;			//Takes a line from the machine instruction file and stores sections into instruction struct. 
 	int op[6] = { };
 	int rs[5] = { };
 	int rt[5] = { };
@@ -251,14 +248,12 @@ instruction decode(string instr) {
 	return instruction_decoded;
 }
 
-void execute(instruction instr, int (&regs)[8], cache& cache, int (&memory)[128]) {
+void execute(instruction instr, int (&regs)[8], cache& cache, int (&memory)[128]) {	//Executes proper function based on instruction opcode.
 	if (instr.op == 35) {
-		//cout << "LW";
 		load_operation(compute_mem_addr(instr), instr.rt, regs, cache, memory);
 
 	}
 	else if (instr.op == 43) {
-		//cout << "SW";
 		store_operation(compute_mem_addr(instr), instr.rt, regs, cache, memory);
 	}
 	else {
@@ -268,22 +263,20 @@ void execute(instruction instr, int (&regs)[8], cache& cache, int (&memory)[128]
 }
 
 
-
-
 int main() {
 	
-	cache cache;
+	cache cache;							//Creates and initializes cache
 	for (int i = 0; i < 8; i++) {
 		cache.block_0[i].set = i;
 		cache.block_1[i].set = i;
 	}
 
-	int memory[128] = { };
+	int memory[128] = { };					//Creates and initializes memory
 	for (int i = 0; i < 128; i++) {
 		memory[i] = i + 5;
 	}
 
-	int reg_file[8] = { };
+	int reg_file[8] = { };					//Creates and initializes registers 
 	
 	string line;
 	ifstream file;
@@ -291,21 +284,21 @@ int main() {
 	file.open("02-Input-object-code");
 
 	
-	while (!file.eof()) {
+	while (!file.eof()) {					//Reading and executing file instructions
 		getline(file, line);
 		cout << line << endl;
 		execute(decode(line), reg_file, cache, memory);
 		cout << "--------------------------------" << endl;
 	}
 	
-	cout << endl << "Registers" << endl;
+	cout << endl << "Registers" << endl;		//Displays registers
 	for (int i = 0; i < 8; i++) {
 		cout << "$S" << i << "\t"; 
 		ThirtyTwoBitBin(reg_file[i]);
 		cout << endl;
 	}
 	
-	cout << endl << "Cache Block 0\t\t\t\t\t\t\tCache Block 1 " << endl;
+	cout << endl << "Cache Block 0\t\t\t\t\t\t\tCache Block 1 " << endl;			//Displays cache
 	cout << "Set#\tValid\tTag\tData\t\t\t\t\tSet#\tValid\tTag\tData" << endl;
 	for (int i = 0; i < 8; i++) {
 		cout << cache.block_0[i].set << "\t" << cache.block_0[i].valid_bit << "\t"; FourBitBin(cache.block_0[i].tag); cout << "\t"; ThirtyTwoBitBin(cache.block_0[i].data);
@@ -313,7 +306,7 @@ int main() {
 		cout << endl;
 	}
 	
-	cout << endl << "Memory" << endl;
+	cout << endl << "Memory" << endl;		//Displays memory
 	cout << "Address\t\tData" << endl;
 	for (int i = 0; i < 128; i++) {
 		SevenBitBin(i);
